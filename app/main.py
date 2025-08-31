9a0554a5701479f9be5b36599467160e7d234971
Vasyl <vasiliy.azarov.vl1@gmail.com>
2025-08-31 19:39:55 +0200

    solution #7

--- app/main.py
+++ app/main.py
@@ -19,16 +19,16 @@ def get_person_from_list_by_name(
 def create_person_list(people_data: List[dict]) -> List[Person]:
     result: List[Person] = [Person(d["name"], d["age"]) for d in people_data]
 
-    for d in people_data:
-        pers = get_person_from_list_by_name(result, d["name"])
+    for dat in people_data:
+        pers = get_person_from_list_by_name(result, dat["name"])
         if pers:
-            if d.get("wife"):
-                wife = get_person_from_list_by_name(result, d["wife"])
+            if dat.get("wife"):
+                wife = get_person_from_list_by_name(result, dat["wife"])
                 if wife:
                     pers.wife = wife
                     wife.husband = pers
-            elif d.get("husband"):
-                husband = get_person_from_list_by_name(result, d["husband"])
+            elif dat.get("husband"):
+                husband = get_person_from_list_by_name(result, dat["husband"])
                 if husband:
                     pers.husband = husband
                     husband.wife = pers
